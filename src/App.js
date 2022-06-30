671aee89d5ceef0283eda7a3fbaa5f641906a164
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-30 10:57:42 -0300

    Fetching Tours

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import Tours from './Tours';
 const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const resp = await fetch(url);
+      const tours = await resp.json();
+      setIsLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
   if (isLoading) {
     return (
       <main>
