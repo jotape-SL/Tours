270798af8fd436a3e43da126b50189d6fb94ed1c
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-01 11:58:32 -0300

    info and price

--- src/Tour.js
+++ src/Tour.js
@@ -4,9 +4,14 @@ const Tour = ({ id, image, info, price, name }) => {
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
-      <div className='tour-info'>
-        <h4>{name}</h4>
-      </div>
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className='delete-btn'>delete</button>
+      </footer>
     </article>
   );
 };
