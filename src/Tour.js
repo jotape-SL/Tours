61d96c1198239565db5b3a15d5ef465b39dc362f
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-01 11:55:41 -0300

    name and image

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <div className='tour-info'>
+        <h4>{name}</h4>
+      </div>
+    </article>
+  );
 };
 
 export default Tour;
