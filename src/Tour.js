bd31abab8bca0cb2607d6c6f8a853cac04f50511
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-02 11:24:47 -0300

    Update Tour.js

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -9,8 +10,15 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className='tour-price'>${price}</h4>
         </div>
-        <p>{info}</p>
-        <button className='delete-btn'>delete</button>
+        <p>
+          {readMore ? info : `${info.substring(0, 210)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'Show less' : 'Read more'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={() => {}}>
+          delete
+        </button>
       </footer>
     </article>
   );
